ee5426dd569dec3e5997f289cc8ee3b0ad27b790
administrator <administrator@ubuntu>
2014-06-26 17:17:27 +0800

    ff

---
+++ git_test/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: administrator
+ *
+ * Created on 2014年6月26日, 下午5:15
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+    int i=9;
+}
+
